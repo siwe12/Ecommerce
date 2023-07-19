de7e8e461f26aa5a9c05b73431d982277864e52f
siwe12 <139961115+siwe12@users.noreply.github.com>
2023-07-19 13:26:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementsByClassName("links").addEventListener("click", click(() => {
+//   console.log("working")
+//   alert("Hello World!");
+// }));
+
+// window.addEventListener("click", function(){
+//   this.alert("worjing")
+// });
+
